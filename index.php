1ffc7d9829705b285ff89a6cb042063300bb8196
Kabuts <saludescleave26@gmail.com>
2022-10-26 23:37:09 +0800

    index.php

--- index.php
+++ index.php
@@ -52,12 +52,31 @@
                         <div class="rower">
                         <div class="row align-items-start">
                             <div class="col">
-                                        <form>
+                                        <form action="" method="post">
+                                            <div class="row flight-type">
+                                                <div class="col-1">
+                                                    <div class="form-check">
+                                                        <input class="form-check-input" type="radio" name="flight_type" id="flexRadioDefault1" value="return"onclick="document.getElementById('return_date').disabled = false;">
+                                                        <label class="form-label" for="flexRadioDefault1">
+                                                          Return
+                                                        </label>
+                                                      </div>
+                                                </div>
+                                                <div class="col-2">
+                                                    <div class="form-check">
+                                                        <input class="form-check-input" type="radio" name="flight_type" id="flexRadioDefault1" value="one_way" 
+                                                        onclick="document.getElementById('return_date').disabled = true;">
+                                                        <label class="form-label" for="flexRadioDefault1">
+                                                        One-Way
+                                                        </label>
+                                                      </div>
+                                                </div>
+                                            </div>
                                             <div class="row rower1">
                                                 <div class="col columns">
                                                     <div class="form-group flight">
                                                         <span class="form-label">Flying from</span>
-                                                        <input class="form-control form-control-sm" list="location" id="input">
+                                                        <input class="form-control form-control-sm" list="location" id="input" name="origin">
                                                         <datalist id="location">
                                                             <?php 
                                                                 $airport = mysqli_query($con, "SELECT * FROM airports");
@@ -71,7 +90,7 @@
                                                 <div class="col columns">
                                                     <div class="form-group flight">
                                                         <span class="form-label">Flyning to</span>
-                                                        <input class="form-control form-control-sm" list="location" id="input">
+                                                        <input class="form-control form-control-sm" list="location" id="input" name="destination">
                                                         <datalist id="location">
                                                             <?php 
                                                                 $airport = mysqli_query($con, "SELECT * FROM airports");
@@ -85,13 +104,13 @@
                                                 <div class="col columns">
                                                     <div class="form-group dates">
                                                         <span class="form-label">Departing</span>
-                                                        <input class="form-control form-control-sm" type="date" >
+                                                        <input class="form-control form-control-sm" type="date" name="departing_date">
                                                     </div>
                                                 </div>
                                                 <div class="col columns dates">
                                                     <div class="form-group">
                                                         <span class="form-label">Returning</span>
-                                                        <input class="form-control form-control-sm" type="date" >
+                                                        <input class="form-control form-control-sm" type="date" name="return_date">
                                                     </div>
                                                 </div>
                                             </div>
@@ -99,7 +118,7 @@
                                                 <div class="col">
                                                     <div class="form-group flight-type">
                                                         <span class="form-label">Trip Type</span>
-                                                        <select class="form-control form-control-sm">
+                                                        <select class="form-control form-control-sm" name="travel_class">
                                                             <option>Return</option>
                                                             <option>One Way</option>
                                                         </select>
@@ -120,19 +139,19 @@
                                                 <div class="col">
                                                     <div class="form-group">
                                                         <span class="form-label">Adults <sub> (18+ years old)</sub></span>
-                                                        <input class="form-control form-control-sm" type="number" placeholder="1" min="1" value="1">
+                                                        <input class="form-control form-control-sm" type="number" name="adult" placeholder="1" min="1" value="1">
                                                     </div>
                                                 </div>
                                                 <div class="col">
                                                     <div class="form-group">
                                                         <span class="form-label">Children <sub>(2 - 12 years old)</sub></span>
-                                                        <input class="form-control form-control-sm" type="number" placeholder="0" min="0">
+                                                        <input class="form-control form-control-sm" type="number" name="child" placeholder="0" min="0">
                                                     </div>
                                                 </div>
                                                 <div class="col">
                                                     <div class="form-group">
                                                         <span class="form-label">Infant <sub>(2 - 12 months)</sub></span>
-                                                        <input class="form-control form-control-sm" type="number" placeholder="0" min="0">
+                                                        <input class="form-control form-control-sm" type="number" name="infant" placeholder="0" min="0">
                                                     </div>
                                                 </div>
                                                 <div class="col">
@@ -167,9 +186,10 @@
                         $return_date = $_POST['return_date'];
                         $adult = $_POST['adult'];
                         $child = $_POST['child'];
+                        $infant = $_POST['infant'];
                         $trip_type = $_POST['trip_type'];
                         $travel_class = $_POST['travel_class'];
-/*
+
                         # Real Time Flight API
                         $queryString = http_build_query([
                             'access_key' => 'ba2a100b516a5eeb4a93b4ce14e1ded6'
@@ -185,38 +205,56 @@
                         curl_close($ch);
                         
                         $flights_data = json_decode($json, true);
-                        */
+                        
                     ?>
                     <div class="container-fluid">
                         <h2>Flights Found</h2>
-                        <div class="table-responsive">
-                            <table>
-                                <thead>
-                                    <tr>
-                                        <th>Departure</th>
-                                        <th colspan="3"><?php echo $flights_data['data']['flight_date']; ?></th>
-                                    </tr>
-                                </thead>
-                                        
-                                <?php foreach($flights_data['data'] as $flights): 
-                                if($flights['flight_status'] == 'scheduled'){
-                                    if ($flights['departure']['iata'] == $origin) {
-                                        if($flights['arrival']['iata'] == $destination) {
-                                            ?>          
+                        <form action="">
+                            <div class="table-responsive">
+                                    <?php foreach($flights_data['data'] as $flights): 
+                                    if($flights['flight_status'] == 'scheduled'){
+                                        if ($flights['departure']['iata'] == $origin) {
+                                            if($flights['arrival']['iata'] == $destination) {
+                                                ?>      
+                                <div class="card">
+                                    <div class="card-body">
+                                        <table>
+                                            <thead>
+                                                <tr>
+                                                    <th>Departure</th>
+                                                    <th colspan="3"><?php echo $flights_data['flight_date']; ?></th>
+                                                </tr>
+                                            </thead>    
                                             <tbody>
                                                 <td class="air text-break"><?php echo $flights['airline']['name']; ?></td>
-                                                <td class="text-right"><input type="radio" name="flight"><span class="text"><strong><?php echo $flights['departure']['iata']. ', ' .$flights['departure']['scheduled']; ?></strong></span><br><p><?php echo $flights['departure']['airport']; ?></p></td>
+                                                <td class="text-right"><span class="text"><strong><?php echo $flights['departure']['iata']. ', ' .$flights['departure']['scheduled']; ?></strong></span><br><p><?php echo $flights['departure']['airport']; ?></p></td>
                                                 <td class="text-center"><?php echo $flights['departure']['scheduled']; ?></td>
                                                 <td class="text-left"><span class="text"><strong><?php echo $flights['arrival']['iata']. ', ' .$flights['arrival']['scheduled']; ?></strong></span><br><p><?php echo $flights['arrival']['airport']; ?></p></td>
                                                 
                                             </tbody>
-                                            <?php
-                                        }
+                                            <thead>
+                                                <tr>
+                                                    <th>Departure</th>
+                                                    <th colspan="3"><?php echo $flights_data['flight_date']; ?></th>
+                                                </tr>
+                                            </thead>    
+                                            <tbody>
+                                                <td class="air text-break"><?php echo $flights['airline']['name']; ?></td>
+                                                <td class="text-right"><span class="text"><strong><?php echo $flights['arrival']['iata']. ', ' .$flights['arrival']['scheduled']; ?></strong></span><br><p><?php echo $flights['arrival']['airport']; ?></p></td>
+                                                <td class="text-center"><?php echo $flights['departure']['scheduled']; ?></td>
+                                                <td class="text-left"><span class="text"><strong><?php echo $flights['departure']['iata']. ', ' .$flights['departure']['scheduled']; ?></strong></span><br><p><?php echo $flights['departure']['airport']; ?></p></td>
+                                                
+                                            </tbody>
+                                        <?php
                                     }
                                 }
-                                endforeach; ?>
-                            </table>
-                        </div>
+                            }
+                            endforeach; ?>
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
                     </div>
                 </main>
             <?php
