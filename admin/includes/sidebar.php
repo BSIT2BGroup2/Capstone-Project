a1b04d9026dad61e772b58fdd19dba1848cb17c9
Kabuts <saludescleave26@gmail.com>
2022-11-04 00:16:17 +0800

    Capstone Project

---
+++ admin/includes/sidebar.php
@@ -0,0 +1,31 @@
+
+
+  <!-- ======= Sidebar ======= -->
+  <aside id="sidebar" class="sidebar">
+
+    <ul class="sidebar-nav" id="sidebar-nav">
+
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="index.html">
+          <i class="bi bi-grid"></i>
+          <span>Dashboard</span>
+        </a>
+      </li><!-- End Dashboard Nav -->
+
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="airport.php">
+          <i class="bi bi-airplane"></i>
+          <span>Airports</span>
+        </a>
+      </li><!-- End Contact Page Nav -->
+
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="users.php">
+          <i class="bi bi-person"></i>
+          <span>Manage Users</span>
+        </a>
+      </li><!-- End Register Page Nav -->
+
+    </ul>
+
+  </aside><!-- End Sidebar-->
