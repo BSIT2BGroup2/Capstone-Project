6e9a6e308ab80c24b18decb1e8d75587885b1f34
Kabuts <saludescleave26@gmail.com>
2022-11-06 09:56:22 +0800

    Capstone Project

--- admin/airport.php
+++ admin/airport.php
@@ -18,65 +18,31 @@
     </div>
 
     <section class="section">
-          <div class="card">
-            <div class="card-body">
-              <h5 class="card-title">Manage Airports</h5>
-
-              <!-- Table with hoverable rows -->
-              <table class="table table-hover">
-                <thead>
-                  <tr>
-                    <th scope="col"></th>
-                    <th scope="col">Name</th>
-                    <th scope="col">Position</th>
-                    <th scope="col">Age</th>
-                    <th scope="col">Start Date</th>
-                    <th scope="">Action</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <th scope="row">1</th>
-                    <td>Brandon Jacob</td>
-                    <td>Designer</td>
-                    <td>28</td>
-                    <td>2016-05-25</td>
-                  </tr>
-                  <tr>
-                    <th scope="row">2</th>
-                    <td>Bridie Kessler</td>
-                    <td>Developer</td>
-                    <td>35</td>
-                    <td>2014-12-05</td>
-                  </tr>
-                  <tr>
-                    <th scope="row">3</th>
-                    <td>Ashleigh Langosh</td>
-                    <td>Finance</td>
-                    <td>45</td>
-                    <td>2011-08-12</td>
-                  </tr>
-                  <tr>
-                    <th scope="row">4</th>
-                    <td>Angus Grady</td>
-                    <td>HR</td>
-                    <td>34</td>
-                    <td>2012-06-11</td>
-                  </tr>
-                  <tr>
-                    <th scope="row">5</th>
-                    <td>Raheem Lehner</td>
-                    <td>Dynamic Division Officer</td>
-                    <td>47</td>
-                    <td>2011-04-19</td>
-                  </tr>
-                </tbody>
-              </table>
-              <!-- End Table with hoverable rows -->
-
+      <div class="row align-items-top">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">Manage Airports <a href="#" class="btn btn-primary">Add Airport</a></h5>
+          </div>
+          <div class="row">
+            <div class="col-lg-3">
+              <div class="card">
+                <img src="assets/img/naia.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">Ninoy Aquino Internationa Airport</h5>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+                <div class="card-footer">
+                <p class="card-text">
+                  <a href="#" class="btn btn-primary">View</a>
+                  <a href="#" class="btn btn-warning">Edit</a>
+                  <a href="#" class="btn btn-danger">Delete</a>
+               </p>
+                </div>
+              </div>
             </div>
           </div>
         </div>
+      </div>
     </section>
 
   </main><!-- End #main -->
